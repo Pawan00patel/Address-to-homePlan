e39f5042a1615ac074aafc5d3d743a01ff29d5fc
Pawan kumar patel R <pavankumarpatil.20cse@cambridge.edu.in>
2023-08-12 21:44:52 +0530

    In this section of code, we aimed to scrape property data from a real estate website using Python and Beautiful Soup, store the collected data in an SQLite database, and create a simple web application using Flask to interact with and visualize the data.

--- Text.py
+++ Text.py
@@ -1,6 +1,6 @@
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
 
 url = 'https://www.magicbricks.com/property-for-sale/residential-commercial-real-estate?proptype=Multistorey-Apartment,Builder-Floor-Apartment,Penthouse,Studio-Apartment,Residential-House,Villa,Residential-Plot,Commercial-Office-Space,Office-ITPark-SEZ,Commercial-Shop,Commercial-Showroom,Commercial-Land,Industrial-Land,Warehouse/Godown,Industrial-Building,Industrial-Shed&BudgetMin=50-Lacs&BudgetMax=1-Crores&cityName=bangalore'
 
@@ -14,23 +14,21 @@ title_elements = soup.find_all(class_='mb-srp__card--title')
 titles = [element.get_text(strip=True) for element in title_elements]
 
 area_elements = soup.find_all('div', class_='mb-srp__card__summary--value')
-areas = [element.get_text(strip=True) for element in area_elements]
+areas = [element.find(string=True, recursive=False).strip() for element in area_elements if 'sqft' in element.text]
 
 description_elements = soup.find_all(class_='two-line-truncated')
-descriptions = [element.get_text(strip=True)[:100] + '...' for element in description_elements]  # Limiting description length to 100 characters
+descriptions = [element.get_text(strip=True)[:100] + '...' for element in description_elements]
 
-data = []
-max_length = max(len(prices), len(titles), len(areas), len(descriptions))
-for i in range(max_length):
-    item = {
-        'Title': titles[i] if i < len(titles) else None,
-        'Price': prices[i] if i < len(prices) else None,
-        'Area': areas[i] if i < len(areas) else None,
-        'Floorplan': descriptions[i] if i < len(descriptions) else None
-    }
-    data.append(item)
+data = list(zip(titles, prices, areas, descriptions))
 
-df = pd.DataFrame(data)
-output_file = 'data.xlsx'
-df.to_excel(output_file, index=False)
-print(f'Data saved to {output_file}')
+conn = sqlite3.connect('property_data.db')
+cursor = conn.cursor()
+
+for item in data:
+    cursor.execute('''
+        INSERT INTO properties (title, price, area, description)
+        VALUES (?, ?, ?, ?)
+    ''', item)
+
+conn.commit()
+conn.close()
---
+++ app.py
@@ -0,0 +1,58 @@
+import sqlite3
+import subprocess
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Function to create a SQLite database and table if they don't exist
+def initialize_database():
+    conn = sqlite3.connect('property_data.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS properties (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            price TEXT,
+            area TEXT,
+            description TEXT
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+initialize_database()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/run_script', methods=['POST'])
+def run_script():
+    try:
+        subprocess.run(['python', 'text.py'])
+        result = 'Script executed successfully.'
+    except Exception as e:
+        result = f'Error: {str(e)}'
+
+    return render_template('index.html', result=result)
+@app.route('/view_data')
+def view_data():
+    conn = sqlite3.connect('property_data.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM properties')
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return render_template('view_data.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
