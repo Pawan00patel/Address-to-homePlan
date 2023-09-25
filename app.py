fce1e23698989c8f9c51cb555c330f3027416576
VivekKumarUpadhyay <vivekkumar.20cse@cambridge.edu.in>
2023-09-25 08:24:25 +0530

    new updated code

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+# app.py
+import time
 import sqlite3
-
-from flask import Flask, render_template, redirect, url_for, request
-from web_scraper import scrape_property_data, insert_data_into_database
+from flask import Flask, render_template, request, redirect, url_for
+from web_scraper import scrape_property_data
 
 app = Flask(__name__)
 
@@ -9,7 +10,6 @@ app = Flask(__name__)
 def initialize_database():
     conn = sqlite3.connect('property_data.db')
     cursor = conn.cursor()
-
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS properties (
             id INTEGER PRIMARY KEY,
@@ -19,28 +19,47 @@ def initialize_database():
             description TEXT
         )
     ''')
-
     conn.commit()
     conn.close()
+
 initialize_database()
+
+# Function to insert data into the database
+def insert_data_into_database(data):
+    try:
+        conn = sqlite3.connect('property_data.db')
+        cursor = conn.cursor()
+        for item in data:
+            cursor.execute('''
+                INSERT INTO properties (title, price, area, description)
+                VALUES (?, ?, ?, ?)
+            ''', (item["title"], item["price"], item["area"], item["description"]))
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(f"An error occurred while inserting data into the database: {str(e)}")
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/scrape', methods=['POST'])
 def scrape():
     try:
         city = request.form['city']
-        city_url = f"https://www.magicbricks.com/property-for-sale/residential-real-estate?cityName={city}"
-
-        scraped_data = scrape_property_data(city_url)
+        start_time = time.time()
+        scraped_data = []
+        while time.time() - start_time < 60 and len(scraped_data) < 400:
+            new_data = scrape_property_data(city)
+            if not new_data:
+                break
+            scraped_data.extend(new_data)
+        
         if scraped_data:
             insert_data_into_database(scraped_data)
-            result = f'Scraped and saved data for {city} successfully.'
+            result = f'Scraped and saved {len(scraped_data)} data entries for {city} successfully.'
         else:
-            result = f'No data found for {city}.'
+            result = f'No data found for {city} within the time limit.'
 
     except Exception as e:
         result = f'Error: {str(e)}'
@@ -51,20 +70,14 @@ def scrape():
 def add_city():
     return render_template('add_city.html')
 
-
-
 @app.route('/view_data')
 def view_data():
     conn = sqlite3.connect('property_data.db')
     cursor = conn.cursor()
-
     cursor.execute('SELECT * FROM properties')
     data = cursor.fetchall()
-
     conn.close()
-
     return render_template('view_data.html', data=data)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- web_scraper.py
+++ web_scraper.py
@@ -1,49 +1,41 @@
+# web_scraper.py
 import requests
 from bs4 import BeautifulSoup
-import sqlite3
 
-def insert_data_into_database(data):
-    try:
-        conn = sqlite3.connect('property_data.db')
-        cursor = conn.cursor()
-
-        for item in data:
-            cursor.execute('''
-                INSERT INTO properties (title, price, area, description)
-                VALUES (?, ?, ?, ?)
-            ''', item)
-
-        conn.commit()
-        conn.close()
-
-    except Exception as e:
-        print(f"An error occurred while inserting data into the database: {str(e)}")
-
-def scrape_property_data(url):
+def scrape_property_data(city):
+    url = f"https://www.magicbricks.com/property-for-sale/residential-real-estate?cityName={city}"
     try:
         response = requests.get(url)
+        response.raise_for_status()
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        price_elements = soup.find_all(class_='mb-srp__card__price--amount')
-        prices = [element.get_text(strip=True) for element in price_elements]
+        # Scrape property data here
 
+        # Example: Extracting property titles
         title_elements = soup.find_all(class_='mb-srp__card--title')
         titles = [element.get_text(strip=True) for element in title_elements]
 
+        # Example: Extracting property prices
+        price_elements = soup.find_all(class_='mb-srp__card__price--amount')
+        prices = [element.get_text(strip=True) for element in price_elements]
+
+        # Example: Extracting property areas
         area_elements = soup.find_all('div', class_='mb-srp__card__summary--value')
         areas = [element.find(string=True, recursive=False).strip() for element in area_elements if 'sqft' in element.text]
 
+        # Example: Extracting property descriptions
         description_elements = soup.find_all(class_='two-line-truncated')
         descriptions = [element.get_text(strip=True)[:100] + '...' for element in description_elements]
 
-        data = list(zip(titles, prices, areas, descriptions))
+        data = []
+        for title, price, area, description in zip(titles, prices, areas, descriptions):
+            data.append({"title": title, "price": price, "area": area, "description": description})
+
         return data
 
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {str(e)}")
         return []
-
-# Example usage with the provided URL:
-url = 'https://www.magicbricks.com/property-for-sale/residential-commercial-real-estate?proptype=Multistorey-Apartment,Builder-Floor-Apartment,Penthouse,Studio-Apartment,Residential-House,Villa,Residential-Plot,Commercial-Office-Space,Office-ITPark-SEZ,Commercial-Shop,Commercial-Showroom,Commercial-Land,Industrial-Land,Warehouse/Godown,Industrial-Building,Industrial-Shed&BudgetMin=50-Lacs&BudgetMax=1-Crores&cityName=bangalore'
-property_data = scrape_property_data(url)
-insert_data_into_database(property_data)
+    except Exception as e:
+        print(f"Error occurred: {str(e)}")
+        return []
